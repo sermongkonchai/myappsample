e2b78c0ffd6a2773d9856c61416cef34f154216b
Sydney Ermongkonchai <sermongk7188@gmail.com>
2019-07-12 09:31:45 -0400

    Starbunks drinks

--- coffeejava.js
+++ coffeejava.js
@@ -1,1 +1,26 @@
+window.onload = function(){
 
+  $donate = document.getElementById("donatehere");
+  $howmanytodonate = document.getElementById("howmanytodonate");
+  $thanks = document.getElementById("Thanks");
+
+
+$donate.onclick = function() {
+   console.log("hi");
+   var amount = document.getElementById("howmanytodonate").value;
+   var saying = `Thank you for giving us ${amount} dollars!`;
+
+   if(amount>100)
+   {
+     console.log("over 100 dollars");
+    saying = `Thank you for giving us ${amount} dollars that's a bucket full of money! You must be rich.`;
+   }
+   if(amount<0)
+   {
+     saying = "Excuse me, you can't give negative dollars. ";
+   }
+    $thanks.innerHTML =`${saying}`;
+
+
+}
+}
